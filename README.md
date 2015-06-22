d4ae62104566c5fe4c68076be60637b482bbee1a
Peter Ajtai <peter.ajtai@thinksolid.com>
2015-06-21 23:04:21 -0700

    Adding file asset handler

---
+++ README.md
@@ -0,0 +1,68 @@
+# Express Asset Handler
+
+`npm install --save express-asset-handler`
+
+This is express middleware that can handle your script and style includes for both your dev and production 
+work flows. It provides direct links to unoptimized files in dev mode, and cache busted links to optimized files in
+optimize (production) mode.
+
+Works only with plain js and css files.
+
+To use require the npm and call with the options object.
+
+```
+// Example usage
+var assetHandlerMiddleware = require('express-asset-handler')({
+    assetsJson : require('./../assets.json'),                               // the json object that describes your assets
+    baseAssetsDir : path.join(__dirname, '..', 'public'),                   
+    optimize : configs.optimize,                                            // true or false
+    optimizedAssetsDir : path.join(__dirname, '..', 'public', 'optimized'), // optimized output directory
+    tmpDir : path.join(__dirname, '..', 'tmp') 
+});
+
+expressApp.use(assetHandlerMiddleware);
+
+// No you can define assets.json that was passed in above:
+// links are as you wish them to appear in the unoptimized version
+
+{
+    "css" : {
+        "main" : [
+            "/css/style1.css",
+            "/css/style2.css"
+        ]
+    }
+    "js" : {
+        "app" : [
+            "/js/theme1.js",
+            "/js/theme2.js"
+        ]
+    }
+}
+
+// And now in any express template you can use
+!= assets.css('main')
+!= assets.js('app')
+```
+
+If `options.optimize` is false, the above will put two script and style tags each in the html. Each tag will point to the
+file referenced in the json.
+
+```html
+<link href="/css/style1.css" rel="stylesheet>
+<link href="/css/style2.css" rel="stylesheet>
+<script src="/js/theme1.js"></script>
+<script src="/js/theme2.js"></script>
+```
+
+If `options.optimize` is true, the above will put one script and style tag each in the html. The tags will point to the
+created min, concated files, and the file name will include the hash of the contents.
+
+```html
+<link href="/optimized/main.12fa0dff2545dbe250b595de818dac6a.min.css" rel="stylesheet">
+<script src="/optimized/app.9079b4287896b7a8e2ef53ddc4d292d1.min.js"></script>
+```
+
+---
+
+More docs, etc. coming later.
---
+++ index.js
@@ -0,0 +1,145 @@
+'use strict';
+
+var _           = require('lodash'),
+    BB          = require('bluebird'),
+    Compressor  = require('node-minify'),
+    fs          = BB.promisifyAll(require('fs')),
+    path        = require('path'),
+    files       = {},       // The cache of optimized file hashes
+    built       = false;    // Whether the optimized file hashes and files were created
+
+module.exports = configure;
+
+function configure(options) {
+
+    // All available options keys are represented below:
+    var defaults = {
+        assetsJson              : null,
+        baseAssetsDir           : null,     // this is the parent dir of the js and css files, e.g. "path.join(__dirname, 'public')"
+        optimize                : false,    // set to true to min, concat, and cache bust
+        optimizedAssetsDir      : null,     // where the optimized assets are put
+        tmpDir                  : null,
+        verbose                 : false
+    };
+
+    options = _.extend({}, defaults, options);
+
+    if (! options.baseAssetsDir) {
+        throw new Error('You must pass in options.baseAssetsDir. e.g. "path.join(__dirname, \'public\')"');
+    }
+
+    if (! options.assetsJson) {
+        throw new Error('You must pass in a JSON object representing you assets groupings. e.g. "require(\'./assets.json\')');
+    }
+
+    return function(req, res, next) {
+
+        // Send functions into jade templates
+        res.locals.assets = {
+            unmodified : {
+                css : css.bind(null, options),
+                js  : js.bind(null, options)
+            },
+            css :   options.optimize ? cssMin.bind(null, files)   : css.bind(null, options),
+            js  :   options.optimize ? jsMin.bind(null, files)    : js.bind(null, options)
+        };
+
+        // Create optimized assets
+        if (options.optimize && !built) {
+            built = true;
+
+            BB.join(
+                minify(options, options.assetsJson.css,  'clean-css',    '.css'),
+                minify(options, options.assetsJson.js,   'uglifyjs',     '.js' ),
+                function(resultsCss, resultsJs) {
+
+                    files.css = resultsCss.reduce(reduceFiles, {});
+                    files.js  = resultsJs.reduce( reduceFiles, {});
+
+                    next();
+                });
+        } else {
+            next();
+        }
+    };
+}
+
+function minify(options, assetsJsonObject, minType, suffix) {
+
+    return BB
+        .all(_.map(assetsJsonObject, function(group, name) {
+
+            var fileOut = path.join(options.optimizedAssetsDir, name + suffix),
+                newFile;
+
+            return new BB(function(resolve) {
+
+                new Compressor.minify({
+                    type        : minType,
+                    fileIn      : _.map(group, function(file) {
+                                    return path.join(options.baseAssetsDir, file);
+                    }),
+                    fileOut     : fileOut,
+                    tempPath    : options.tmpDir,
+                    callback    : function(err, file) {
+
+                        var hash = require('crypto').createHash('md5').update(file).digest('hex');
+
+                        // Add the hash of the file contents to the file name
+                        newFile = fileOut.split('.');
+                        newFile.splice(-1,0, hash + '.min');
+                        newFile = newFile.join('.');
+
+                        if (options.verbose) {
+                            console.log('old', fileOut);
+                            console.log('new', newFile);
+                        }
+
+                        fs
+                            .renameAsync(fileOut, newFile)
+                            .then(function() {
+                                resolve({
+                                    name : name,
+                                    hash : hash
+                                });
+                            });
+                    }
+                });
+            });
+        }));
+}
+
+function css(options, group) {
+    return options.assetsJson
+        .css[group]
+        .reduce(function(output, file) {
+            return output + '<link href="' + file + '" rel="stylesheet">';
+        }, '');
+}
+
+function js(options, group) {
+    return options.assetsJson
+        .js[group]
+        .reduce(function(output, file) {
+            return output + '<script src="' + file + '"></script>';
+        }, '');
+}
+
+function cssMin(files, group) {
+    var file = '/optimized/' + group + '.' + files.css[group].hash + '.min.css';
+    return '<link href="' + file + '" rel="stylesheet">';
+}
+
+function jsMin(files, group) {
+    var file = '/optimized/' + group + '.' + files.js[group].hash + '.min.js';
+    file = '<script src="' + file + '"></script>';
+    return file;
+}
+
+function reduceFiles(allFiles, result) {
+    allFiles[result.name] = {
+        hash : result.hash
+    };
+    return allFiles;
+}
+
